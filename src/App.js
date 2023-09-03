ec08d10c6000470c9b555a4544249560ff6aa35c
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-04 02:14:59 +0530

    initial setup APP

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Randomuser from './Components/RabdomUser/Randomuser';
+
+function App() {
+  return (
+    <div className="App">
+          <Randomuser/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/RabdomUser/Randomuser.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Randomuser = () => {
+  return (
+    <>
+      <div className='wrp'>
+    
+
+        <div className='user_ran'>
+          <img src='https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/11/hfpqyV7B-IMG-Dubai-UAE-1200x800.jpg' />
+          <h6>Hello </h6>
+          <p>loremsdfsdk weruiwjh</p>
+          <button>Generate User</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Randomuser
